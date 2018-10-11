ceb369d19f1cfd78270dbaafe41c71a6ef0c5160
Gábor Lászlò Hajba <gabor.hajba@gmail.com>
2018-10-11 14:47:17 +0200

    switched from deprecated method

--- 12_bootstrap/src/main/java/hibernate_example/Main.java
+++ 12_bootstrap/src/main/java/hibernate_example/Main.java
@@ -3,6 +3,8 @@ package hibernate_example;
 import java.text.MessageFormat;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -14,11 +16,13 @@ public class Main {
     public static void main(String[] args) {
         final SessionFactory factory = HibernateUtil.createSessionFactory();
         final Session session = factory.openSession();
-        final Book book = new Book("9781617291999 ", "Java 8 in Action", "Raoul-Gabriel Urma, Mario Fusco, and Alan Mycroft");
+        final Book book = new Book("9781617291999", "Java 8 in Action", "Raoul-Gabriel Urma, Mario Fusco, and Alan Mycroft");
         session.beginTransaction();
         session.save(book);
         session.getTransaction().commit();
-        final List<Book> books = session.createCriteria(Book.class).list();
+        CriteriaQuery<Book> query = session.getCriteriaBuilder().createQuery(Book.class);
+        query.from(Book.class);
+        final List<Book> books = session.createQuery(query).getResultList();
         System.out.println("\n----\n");
         System.out.println(MessageFormat.format("Storing {0} books in the database", books.size()));
         for (final Book b : books) {
